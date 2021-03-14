8e6cfdbab72b340f2324c88370baf14e44118506
Adrian Eguez <eadepto@hotmail.com>
2021-03-14 18:38:06 -0500

    cambios en interface

--- generator-man-lab-yo-ng/generators/v2-ruta/templates/.interface.ts
+++ generator-man-lab-yo-ng/generators/v2-ruta/templates/.interface.ts
@@ -2,5 +2,5 @@
 export interface <%= nombreMayuscula %>Interface extends EntidadComunProyecto{
   <%= id %>?: number;
   // nombreCampo?: string; // Cada uno de los campos de la Entidad
-  //
+  // nombreCampoRelacion?: CampoRelacionInterface | number;
 }
