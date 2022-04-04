a7987c52947652af771adaabd1dcbd006a3f8e56
Adrian <eadepto@hotmail.com>
2022-04-04 11:28:57 -0500

    update/0.12.5 Arreglar bugs

--- generator-man-lab-yo-ng/generators/java-ruta/index.js
+++ generator-man-lab-yo-ng/generators/java-ruta/index.js
@@ -228,7 +228,7 @@ module.exports = class extends Generator {
 
 
         const templateMapper = this.templatePath(TEMPLATES.MAPPER);
-        const destinoMapper = this.destinationPath(`./dao/${nombreMayuscula}Mapper.java`);
+        const destinoMapper = this.destinationPath(`./mapper/${nombreMayuscula}Mapper.java`);
 
         this.fs.copyTpl(
             templateMapper,
